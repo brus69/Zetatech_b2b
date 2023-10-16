1e139f1465cd4737cb64655d242cb4bf102d7892
jane2yana <jane.jana7@gmail.com>
2023-10-16 15:06:15 +0300

    Add blog/tagblog models and admin

--- backend/zetatech_backend/apps/blog/admin.py
+++ backend/zetatech_backend/apps/blog/admin.py
@@ -1,3 +1,42 @@
+from datetime import datetime
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blog, TagBlog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'author',
+        'published',
+        'pub_date',
+        'short_title'
+    )
+
+    list_editable = ('published',)
+    list_filter = ('author', 'tags')
+    search_fields = ('title', 'author', 'tags')
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        obj.save()
+        form.save_m2m()
+
+        if (obj.pub_date is None
+                and obj.published
+                and 'published' in form.changed_data):
+            obj.pub_date = datetime.now()
+            obj.save()
+            form.save()
+
+
+@admin.register(TagBlog)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'slug',
+    )
+    list_filter = ('name',)
--- backend/zetatech_backend/apps/blog/models.py
+++ backend/zetatech_backend/apps/blog/models.py
@@ -1,3 +1,77 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class TagBlog(models.Model):
+    """Модель тегов. Состоит из полей: имя, цвет и слаг."""
+    name = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name='Название тега'
+    )
+    slug = models.SlugField(
+        max_length=150,
+        unique=True,
+        verbose_name='Слаг тега'
+    )
+
+    class Meta:
+        ordering = ('name',)
+        default_related_name = 'blog_tags'
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Blog(models.Model):
+    """Модель блога."""
+    title = models.CharField(
+        max_length=150,
+        verbose_name='Заголовок'
+    )
+    description = models.CharField(
+        max_length=400,
+        verbose_name='Описание'
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Создан'
+    )
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.PROTECT,
+        editable=False
+    )
+    image = models.ImageField(
+        upload_to='apps/blog/media',
+        verbose_name='Изображение',
+        blank=True
+    )
+    content = models.TextField(
+        verbose_name='Текст публикации'
+    )
+    tags = models.ManyToManyField(
+        TagBlog,
+        verbose_name='Теги'
+    )
+    published = models.BooleanField(
+        default=False,
+        verbose_name='Опубликован'
+    )
+    pub_date = models.DateTimeField(
+        default=None,
+        null=True,
+        verbose_name='Дата публикации',
+        editable=False
+        )
+
+    class Meta:
+        default_related_name = 'blogs'
+        verbose_name = 'Публикация'
+        verbose_name_plural = 'Публикации'
+
+    @property
+    def short_title(self):
+        return self.title if len(self.title) < 30 else (self.title[:28] + '..')
