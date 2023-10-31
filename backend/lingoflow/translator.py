cd7f51f2f167a43907b209e11512a3f0aefaa30f
Andrey Brusilov <brus739@gmail.com>
2023-10-31 22:47:16 +0300

    Add lingflow

--- backend/lingoflow/translator.py
+++ backend/lingoflow/translator.py
@@ -1,3 +1,12 @@
 """
 Модуль для взаимодействия с DeepL API для перевода текста.
+1) открыть json файл
+2) отправить запрос
+3) получить ответ
 """
+import json
+
+with open('test_data_json/data.json') as f:
+    data = json.load(f)
+
+print (data)
