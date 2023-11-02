163d38189e6d10f2350adc6aa363c6a1338ed145
Casey <arramine@mail.ru>
2023-11-02 20:40:54 +0300

    fix

--- web/src/features/home/ui/promo.tsx
+++ web/src/features/home/ui/promo.tsx
@@ -10,7 +10,7 @@ export const Promo = () => {
 
   return (
     <section className="container flex flex-col mt-16 mb-32">
-      <Title className="mb-10 max-w-[1100px] text-black text-4xl sm:text-6xl lg:text-[80px]" order={1}>
+      <Title className="mb-10 max-w-[1090px] text-black text-4xl sm:text-6xl lg:text-[80px]" order={1}>
         Парсинг сайтов, магазинов и маркетплейсов{" "}
       </Title>
 
