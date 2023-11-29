f328ad1986cf20cb9185dd0566d174bd05049810
Andrey Brusilov <brus739@gmail.com>
2023-11-30 02:31:39 +0300

    fix base test

--- backend/lingoflow/database.py
+++ backend/lingoflow/database.py
@@ -2,3 +2,16 @@
 Модуль для взаимодействия с базой данных, 
 где вы будете хранить переведенные статьи
 """
+from sqlalchemy import create_engine, Column, Integer, String, Text
+from sqlalchemy.orm import declarative_base
+Base = declarative_base()
+
+class TranslatedText(Base):
+    __tablename__ = 'translated_texts'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    description = Column(String)
+    h1 = Column(String)
+    content = Column(Text)
+
