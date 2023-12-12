4b868ac1bec4e154c1bd3068909c49d2bb499ec5
Andrey Brusilov <brus739@gmail.com>
2023-12-12 12:51:21 +0300

    fix database

--- backend/lingoflow/translator.py
+++ backend/lingoflow/translator.py
@@ -9,21 +9,21 @@ import sqlite3
 
 from constants import DEEPL_TOKEN
 
+headers = {"Authorization":f"DeepL-Auth-Key !!!ade09c63-1962-3ebb-020d-ef5adedb2036:fx"}
 
-headers = {"Authorization":f"DeepL-Auth-Key {DEEPL_TOKEN}"}
-
-def base_connect():
-    """Показывает кол-во символов для превода, и сам текст для превода"""
+def base_connect() -> list[tuple]:
+    """Показывает текст для превода"""
     conn = sqlite3.connect('crawler/sqlite.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM lingflow_scrapy")
+    cursor.execute("SELECT title, description, h1, content FROM lingflow_scrapy LIMIT 2")
     data_list = cursor.fetchall()
     # result_dict = {item[0]: (item[1], item[2], item[3], item[4]) 
     #                for item in data_list}
     cursor.close()
     return data_list
 
-def quantity_text(text):
+
+def quantity_text(text:str) -> int:
     """подсчет кол-ва символов для перевода"""
     count_symbol = 0
     for i in text:
@@ -32,21 +32,25 @@ def quantity_text(text):
                     count_symbol += len(n)
     return count_symbol
     
-
-def translittext(text):
+def translittext(text:str) -> str:
     """Перевод текста"""
+    # Ответ {"translations":[{"detected_source_language":"EN","text":"Что такое веб-скрепинг"}]}
     url = 'https://api-free.deepl.com/v2/translate'
     payload = {"text":text,"target_lang":"RU"}
     r = requests.post(url, headers=headers, data=payload)
-    return r.text
+    if r.status_code == 403:
+        return f'Доступ запрещен к {url} код ошибки: {r.status_code}'
+    else:
+        return r.text
+    
 
-def limittext():
+def limittext() -> dict:
     """Проверка лимитов на стороне deepl"""
     url = 'https://api-free.deepl.com/v2/usage'
     r = requests.get(url, headers=headers)
     return r.json()
 
-def checklimittranslit(count_symbol, limittranslit):
+def checklimittranslit(count_symbol:int, limittranslit:int) -> bool:
     """Проверка на возможность перевести текст с текущим лимитом"""
     result = limittranslit['character_limit'] - limittranslit['character_count']
     if count_symbol > result:
@@ -54,3 +58,4 @@ def checklimittranslit(count_symbol, limittranslit):
         return False
     else:
         return True
+
