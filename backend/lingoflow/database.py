a9f4d3e95a2db823043ddcec2e38a619a7bbae9a
Andrey Brusilov <brus739@gmail.com>
2024-01-04 22:19:49 +0300

    new code type

--- backend/lingoflow/database.py
+++ backend/lingoflow/database.py
@@ -3,16 +3,26 @@
 где вы будете хранить переведенные статьи
 """
 import sqlite3
-import requests
+from datetime import datetime
 
-DEEPL_API_KEY = ''
-DEEPL_API_URL = 'https://api-free.deepl.com/v2/translate'
+DATA_BASE = 'crawler/sqlite.db'
 
 
-def create_translated_table(cursor):
+def base_connect() -> list[tuple]:
+    """Соеденение с БД для перевода"""
+    conn = sqlite3.connect(DATA_BASE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, title, description, h1, content FROM lingflow_scrapy LIMIT 2")
+    data_list = cursor.fetchall()
+    cursor.close()
+    return data_list
+
+
+def create_translated_table(cursor, table_name = 'translated_articles'):
     """Создает таблицу для переведенных статей"""
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS translated_articles (
+
+    cursor.execute(f'''
+        CREATE TABLE IF NOT EXISTS {table_name} (
             id INTEGER PRIMARY KEY,
             title TEXT,
             description TEXT,
@@ -25,64 +35,22 @@ def create_translated_table(cursor):
         )
     ''')
 
-def translate_text(text):
-    """Перевод текста с использованием DeepL API"""
-    if text:
-        payload = {
-            'text': text,
-            'target_lang': 'ru',
-            'auth_key': DEEPL_API_KEY
-        }
 
-        response = requests.post(DEEPL_API_URL, data=payload)
+def _create_name_bd(url: str) -> str:
+    """Создает название таблицы для перевода
+       на основе url адреса и текущей даты
+    """
+    name_domain = url.split("/")[2]
+    name_domain_clear = name_domain.replace(".", "_")
+    if name_domain.find("-") != -1:
+        name_domain_clear = name_domain_clear.replace("-", "_")
+    now = datetime.now()
+    date = now.strftime("%d_%m_%Y")
+    table_name = f'translated_{name_domain_clear}_{date}'
+    return table_name
 
-        if response.status_code == 200:
-            translation_data = response.json()
-            translated_text = translation_data['translations'][0]['text']
-            return translated_text
-        else:
-            print(f"Ошибка при переводе текста. Код ошибки: {response.status_code}")
-    return None
-
-def main():
-    # Подключение к исходной базе данных
-    source_connection = sqlite3.connect('crawler/sqlite.db')
-    source_cursor = source_connection.cursor()
-
-    # Подключение к новой базе данных
-    translite_connection = sqlite3.connect('translite.db')
-    translite_cursor = translite_connection.cursor()
-
-    # Создание таблицы для переведенных статей
-    create_translated_table(translite_cursor)
-    translite_connection.commit()
-
-    # Перевод и сохранение в новую базу данных
-    source_cursor.execute('SELECT id, title, description, h1, content FROM lingflow_scrapy')
-    articles = source_cursor.fetchall()
-
-    for article in articles:
-        article_id, title, description, h1, content = article
-
-        translated_title = translate_text(title)
-        translated_description = translate_text(description)
-        translated_h1 = translate_text(h1)
-        translated_content = translate_text(content)
-
-        translite_cursor.execute('''
-            INSERT INTO translated_articles (
-                id, title, description, h1, content,
-                translated_title, translated_description, translated_h1, translated_content
-            )
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ''', (article_id, title, description, h1, content,
-              translated_title, translated_description, translated_h1, translated_content))
-
-    translite_connection.commit()
-
-    # Закрытие соединений
-    source_connection.close()
-    translite_connection.close()
 
 if __name__ == "__main__":
-    main()
+
+    url = 'https://docs.pyt-hon.org/3/library/datetime.html'
+    print(_create_name_bd(url))
---
+++ backend/lingoflow/exceptions.py
@@ -0,0 +1,10 @@
+class CantGetTranslated(Exception):
+    """Отказ в переводе"""
+
+
+class ExceededGetLimit(Exception):
+    """Превышен лимит"""
+
+
+class CantGetLimit(Exception):
+    """Отказ в получении лимитов"""
